41a86e38727bd06070b56cba3f72c8c38f2e7f4c
riccabolla <bollach@virgilio.it>
2024-09-16 12:52:40 +0200

    Update quality.sh

--- script/quality.sh
+++ script/quality.sh
@@ -1,1 +1,26 @@
+#!/bin/bash	
+
+input_dir="sra/"
+output_dir="sra_filtered"
+
+source activate qcheck
+
+for file in sra/*_R1.fastq.gz; do
+    sample_name=$(basename $file _R1.fastq.gz)
+    fastp -i $file -I sra/${sample_name}_R2.fastq.gz -o sra_filtered/${sample_name}_R1_filtered.fastq.gz -O sra_filtered/${sample_name}_R2_filtered.fastq.gz -q 30
+
+done
+cd sra_filtered/
+
+fastqc *_filtered.fastq.gz 
+
+multiqc .
+
+conda deactivate 
+
+mv *html QC/
+
+done
+
+echo done!
 
