9098a968ddb32a084ab7b3abbe8beb9285fd539f
riccabolla <bollach@virgilio.it>
2024-10-07 11:42:03 +0200

    Update checkM.R

--- Statistical_analysis/checkM.R
+++ Statistical_analysis/checkM.R
@@ -1,13 +1,39 @@
 library(readxl)
 library(tidyverse)
 
-data <- read_xlsx("checkM.xlsx")
-data$Completeness <- as.numeric(data$Completeness)
-data$Contamination <- as.numeric(data$Contamination)
-data$`Strain heterogeneity` <- as.numeric(data$`Strain heterogeneity`)
-str(data$Contamination)
-data <- data %>%
-  select(Completeness, Contamination, `Strain heterogeneity`, gene_0, gene_1, gene_2,gene_3) %>%
+checkm_data <- read_xlsx("checkM.xlsx")
+checkm_data[,3:ncol(checkm_data)] <- lapply(checkm_data[,3:ncol(checkm_data)], as.numeric)
+checkm_data <- checkm_data %>%
+  select(`Bin Id`,Completeness, Contamination, `Strain heterogeneity`, gene_0, gene_1, gene_2,gene_3)
+checkm_data_long <- checkm_data %>% 
+  pivot_longer(cols = -`Bin Id`, names_to = "CheckM parameter", values_to = "val") 
+
+quast_lab <- c(
+  'Completeness (%)' = 'Completeness (%)',
+  'Contamination (%)' = 'Contamination (%)' ,
+  'gene_0' = '0 gene copy',
+  'gene_1' = '1 gene copies',
+  'gene_2' = '2 gene copies',
+  'gene_3' = '3 gene copies',
+  'Strain heterogeneity' = 'Strain heterogeneity (%)'
+)
+
+ggplot(checkm_data_long, aes(x=`CheckM parameter`, y=val)) +
+  geom_boxplot(outliers = FALSE, width=1, aes(fill= `CheckM parameter`)) +
+  geom_jitter(shape=21 ,fill = 'lightgrey', height=0, width=0.1) +
+  theme_classic() +
+  facet_wrap(~ `CheckM parameter`, scales='free_y', labeller = labeller(`CheckM parameter` = quast_lab), ncol=3) +  # Facet by 'type' and control columns
+  ylab('') + xlab('') +
+  scale_fill_brewer(palette="Set1")+
+  theme(
+    axis.text.x = element_blank(),  
+    axis.ticks.x = element_blank(), 
+    text = element_text(size=12),
+    strip.text = element_text(size=12))
+
+ggsave('CheckM_plots.jpg', dpi = 300, path = 'Plot/')
+
+checkm_data <- checkm_data %>%
   summarise(median_completeness = median(Completeness),
             IQR_completeness = IQR(Completeness),
             median_contamination = median(Contamination),
