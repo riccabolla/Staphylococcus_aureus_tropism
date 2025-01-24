c817724219fd90b800767fb4cb9ed7d32fd308ae
riccabolla <bollach@virgilio.it>
2025-01-24 22:27:58 +0100

    Update required_packages.R

--- Statistical_analysis/required_packages.R
+++ Statistical_analysis/required_packages.R
@@ -1,3 +1,1 @@
-packages <- c("readxl", "tidyverse", "gridExtra", "ggplot2","circlize", "ComplexHeatmap", "DescTools", "caret" , "pROC", "ggsignif", "FSA")
-
-install.packages(packages)
+install.packages(c("readxl", "tidyverse", "gridExtra", "ggplot2","circlize", "ComplexHeatmap", "DescTools", "caret" , "pROC", "ggsignif", "FSA"))
